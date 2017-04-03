056476a45bb53d6df13db9eb97107c1e46276efe
James Coliz <jcoliz@mail.com>
2017-04-03 08:00:19 -0700

    Code cleanup refactor

--- IotHello.Uwp/Converters/BooleanBrushConverter.cs
+++ IotHello.Uwp/Converters/BooleanBrushConverter.cs
@@ -1,49 +1,26 @@
-﻿using System;
+﻿using ManiaLabs.Portable.Base.ValueConverters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media;
 
 namespace IotHello.Uwp.Converters
 {
-    class BooleanBrushConverter : IValueConverter
+    public class BooleanRedBrushConverter : DefaultConverter
     {
-        public object Convert(object value, Type targetType, object parameter, string language)
+        SolidColorBrush Red = new SolidColorBrush(Colors.Red);
+        SolidColorBrush Black = new SolidColorBrush(Colors.Black);
+        public override object Convert(object value, Type targetType, object parameter)
         {
-            // Paramter should be a comma-separated set of strings containing values in the Windows.UI.Colors enumeration
-            if (parameter == null)
-                throw new ArgumentNullException(nameof(parameter));
-
-            var parameter_string = parameter as string;
-            var parameter_array = parameter_string.Split(',');
-    
-            if (parameter_array.Count() != 2)
-                throw new ArgumentException("Expected: 'AARRGGBB,AARRGGBB'",nameof(parameter));
-
-            if (!(value is Boolean))
-                throw new ArgumentException("Expected: Boolean", nameof(value));
-
-            var value_bool = (Boolean)value;
-
-            string which_parameter = parameter_array[1];
-            if (value_bool)
-                which_parameter = parameter_array[0];
-
-            var argb = System.Convert.ToUInt32(which_parameter,16);
-            var color = Windows.UI.ColorHelper.FromArgb((byte)(argb >> 24),(byte)((argb >> 16) & 0xff),(byte)((argb >> 8) & 0xff),(byte)(argb & 0xff));
-            var brush = new SolidColorBrush(color);
-
-            if (targetType != typeof(Brush))
-                throw new ArgumentException("Expected: Brush", nameof(targetType));
-
-            return brush;
+            return base.Convert<Brush>(Black, Red, value, targetType, parameter);
         }
+    }
 
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            throw new NotImplementedException();
-        }
+    public class BooleanRedBrushConverterXaml : Platform.XamlValueConverter<BooleanRedBrushConverter>
+    {
     }
 }
--- IotHello.Uwp/Converters/DateFormatConverter.cs
+++ IotHello.Uwp/Converters/DateFormatConverter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IotHello.Uwp.Platform;
+using ManiaLabs.Portable.Base.ValueConverters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,46 +9,10 @@ using Windows.UI.Xaml.Data;
 
 namespace IotHello.Uwp.Converters
 {
-    public class DateFormatConverter: IValueConverter
+    public class DateFormatConverterXaml : XamlValueConverter<DateFormatConverter>
     {
-        public object Convert(object value, Type targetType, object parameter, string culture)
-        {
-            String format = null;
-            if (parameter != null && (String)parameter != null)
-                format = (String)parameter;
-
-            String result = "---";
-            if (value is DateTime?)
-            {
-                var input = value as DateTime?;
-
-                if (input.HasValue)
-                {
-                    if (format != null)
-                        result = input.Value.ToString(format);
-                    else
-                        result = input.Value.ToString();
-                }
-
-            }
-            else if (value is TimeSpan?)
-            {
-                var input = value as TimeSpan?;
-
-                if (input.HasValue)
-                {
-                    if (format != null)
-                        result = input.Value.ToString(format);
-                    else
-                        result = input.Value.ToString();
-                }
-            }
-            return result;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, string culture)
-        {
-            throw new NotImplementedException();
-        }
+    }
+    public class TimeSpanFormatConverterXaml : XamlValueConverter<TimeSpanFormatConverter>
+    {
     }
 }
