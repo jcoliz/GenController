7964d89988b8b19c255c664c9f715b276b514260
James Coliz <jcoliz@mail.com>
2017-04-18 23:29:34 -0700

    Added timers. Looks done now!

--- GenController.MockHardware/StartupTask.cs
+++ GenController.MockHardware/StartupTask.cs
@@ -15,8 +15,13 @@ namespace GenController.MockHardware
     {
         BackgroundTaskDeferral Deferral;
         ThreadPoolTimer Timer;
+        ThreadPoolTimer StopButtonTimer;
+        ThreadPoolTimer StartButtonTimer;
         Input StopButton;
         Input StartButton;
+        DirectLight PanelLight;
+        DirectLight RunSignalLight;
+        DirectLight PowerLight;
 
         const int run_signal_pin = 24;
         const int panel_light_pin = 25;
@@ -24,20 +29,62 @@ namespace GenController.MockHardware
         const int stop_autolight_pin = 18;
         const int stop_button_pin = 20;
         const int start_button_pin = 21;
+        const int power_light_pin = 16;
 
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             Deferral = taskInstance.GetDeferral();
 
-            var StopAutoLight = new DirectLight(stop_autolight_pin);
-            var StartAutoLight = new DirectLight(start_autolight_pin);
+            var StopAutoLight = new DirectLight(stop_autolight_pin) { State = false };
+            var StartAutoLight = new DirectLight(start_autolight_pin) { State = false };
+
+            RunSignalLight = new DirectLight(run_signal_pin) { State = false };
+            PanelLight = new DirectLight(panel_light_pin) { State = false };
+            PowerLight = new DirectLight(power_light_pin) { State = true };
 
             StopButton = new Input(stop_button_pin,StopAutoLight,false);
             StopButton.Updated += (s, e) => 
             {
-
+                // If pressed
+                if (StopButton.State == false)
+                {
+                    RunSignalLight.State = false;
+                    PanelLight.State = false;
+                    StopButtonTimer = ThreadPoolTimer.CreateTimer((a) => 
+                    {
+                        PanelLight.State = true;
+                    }, TimeSpan.FromSeconds(5));
+                }
+                else
+                {
+                    if (StopButtonTimer != null)
+                    {
+                        StopButtonTimer.Cancel();
+                        StopButtonTimer = null;
+                    }
+                }
             };
             StartButton = new Input(start_button_pin,StartAutoLight,false);
+            StartButton.Updated += (s, e) =>
+            {
+                // If pressed
+                if (StartButton.State == false)
+                {
+                    StartButtonTimer = ThreadPoolTimer.CreateTimer((a) =>
+                    {
+                        RunSignalLight.State = true;
+                        PanelLight.State = true;
+                    }, TimeSpan.FromSeconds(5));
+                }
+                else
+                {
+                    if (StartButtonTimer != null)
+                    {
+                        StartButtonTimer.Cancel();
+                        StartButtonTimer = null;
+                    }
+                }
+            };
 
             Timer = ThreadPoolTimer.CreatePeriodicTimer(Tick, TimeSpan.FromMilliseconds(100));
         }
