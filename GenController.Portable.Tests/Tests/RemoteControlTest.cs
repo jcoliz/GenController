9cfe8af461e9287bc0e466d6c06674a646682dea
James Coliz <jcoliz@mail.com>
2018-01-21 20:53:18 -0800

    Added new stub class, with test, RemoteControl. To build out a latching remote control interface.

---
+++ GenController.Portable.Tests/Tests/RemoteControlTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GenController.Portable.Tests.Mocks;
+using GenController.Portable.Models;
+using Common;
+
+namespace IotHello.Portable.Tests.Tests
+{
+    [TestClass]
+    public class RemoteControlTest
+    {
+        private MockController Controller;
+        private RemoteControl RC;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            Service.Set<ISettings>(new MockSettings());
+            Controller = new MockController();
+            GenController.Portable.Models.Controller.Current = Controller;
+            RC = new RemoteControl();
+        }
+
+        [TestMethod]
+        public void Empty()
+        {
+            Assert.IsNotNull(RC);
+        }
+    }
+}
---
+++ GenController.Portable/Models/RemoteControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenController.Portable.Models
+{
+    /// <summary>
+    /// Provides a way to control the generator remotely using an RF remote
+    /// </summary>
+    /// <remarks>
+    /// This is built with a latching RF receiver with two lines. A "start" button
+    /// line and a "stop" button line. We act when there are changes in those lines.
+    /// </remarks>
+    public class RemoteControl
+    {
+    }
+}
